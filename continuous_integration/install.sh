c61b4bd11e242637793fca79aeb6a42cae0119e6
Alan Chan <github@zenan.ch>
2017-10-30 15:18:02 +0800

    Adding a notebook on an experimental approach. (#16)
    * adds a notebook on an experimental approach.
    
    * fixes a Trivis CI dependency issue
    
    * fixes another mysterious bug with Travis CI
    
    * adds boto to dependencies
    
    * upgrades gensim to see if it solves the issue
    
    * removes unhelpful procedure
    
    * adopts travis config from gensim
    
    * fixes a typo of the path
    
    * lets numpy install via conda
    
    * Upgrade to Python 3.6

---
+++ continuous_integration/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Do NOT run this on your PC. This is meant to be run on a virtual machine for testing purpose.
+
+set -e
+
+deactivate
+wget 'http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh' -O miniconda.sh
+chmod +x miniconda.sh && ./miniconda.sh -b
+export PATH=/home/travis/miniconda2/bin:$PATH
+conda update --yes conda
+
+
+conda create --yes -n fmr-test python=3.6 pip atlas flake8 numpy==1.12.0 && source activate fmr-test
+pip install -r requirements.txt
+cd trained
+python download.py
+cd ..
+python manage.py create_table
